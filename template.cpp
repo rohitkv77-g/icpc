bb776334f5f5d8f968e0f953741be3e877c97908
Rohit verma <rohitkverma77@gmail.com>
2020-09-14 14:00:08 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ALL(a) a.begin(), a.end()
+#define RALL(a) a.rbegin(), a.rend()
+#define pb push_back
+#define MOD 1000000007
+#define sz5 100005
+#define sz6 1000005
+#define UP upper_bound
+#define LB lower_bound
+#define F first
+#define S second
+#define PI pair<int,int>
+#define PL pair<ll,ll>
+#define VI vector<int>
+#define VL vector<ll>
+
+void test_case(void) {
+
+}
+
+int main() {
+	FAST
+
+	#ifndef ONLINE_JUDGE
+		freopen("in", "r", stdin);
+		// freopen("out", "w", stdout);
+	#endif
+
+	int t;  cin>>t;
+	for(int i=1;i<=t;i++){
+		// cout<<"Case #"<<i<<": ";
+		test_case();
+	}
+
+	return 0;
+}
